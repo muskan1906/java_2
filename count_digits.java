b4f9e9fda29c733a616d295b6cebd8f80ce4c12d
muskan1906 <122630113+muskan1906@users.noreply.github.com>
2023-10-04 13:44:43 +0530

    main

---
+++ count_digits.java
@@ -0,0 +1,17 @@
+import java.util.*;
+    public class count_digits {
+        public static void main(String args[]) {
+            int c=0,N,Digit;
+            Scanner s=new Scanner(System.in);
+            N=s.nextInt();
+            Digit=s.nextInt();
+            while(N>0){
+                if(N%10==Digit)
+                c+=1;
+                N/=10;
+            }
+            System.out.println(c);
+    
+        }
+    }
+
