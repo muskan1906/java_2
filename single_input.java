d2270626f8cfaed0f91592014bf820e86373bf96
muskan1906 <122630113+muskan1906@users.noreply.github.com>
2023-10-04 13:26:32 +0530

    main

---
+++ single_input.java
@@ -0,0 +1,16 @@
+import java.util.*;
+    public class single_input {
+        public static void main(String args[]) { 
+            Scanner sc = new Scanner(System.in);
+            int a = 0;
+            while(a>=0){
+                int n = sc.nextInt();
+                a=a+n;
+                if(a>=0){
+                    System.out.println(n);
+                }
+            }
+    
+        }
+    } 
+
